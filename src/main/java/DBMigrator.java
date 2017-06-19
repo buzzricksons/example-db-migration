cbae2b584812ebded128ddb5f1d0f2ce79e0d0bc
buzzricksons <hcgoon@gmail.com>
2017-06-19 23:51:27 +0900

    add migration method

--- src/main/java/DBMigrator.java
+++ src/main/java/DBMigrator.java
@@ -1,13 +1,25 @@
 import org.flywaydb.core.Flyway;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
 /**
  * DBマイグレーションを行います。
  * このクラスはイミュータブルではありません。マイグレーションが実行されるタイミングで結果が変わる場合があります。
  *
  * @author HyungCheol Kim
  */
+@Data
+@Builder
 public class DBMigrator {
+    @NonNull
     private Flyway flyway;
+    @NonNull
     private DBMigratorInfo info;
+
+    public int migrate() {
+        return flyway.migrate();
+    }
 
 }
